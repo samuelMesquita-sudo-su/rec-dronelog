3aa023db54866dace869703b4550467684ddfb2e
samuel@SudoSu <samuel_mesquita@estudante.sesisenai.org.br>
2026-06-09 21:14:26 -0300

    outro commit

--- src/main/java/org/example/dronelog/repository/MissaoVooRepository.java
+++ src/main/java/org/example/dronelog/repository/MissaoVooRepository.java
@@ -1,15 +1,24 @@
 package org.example.dronelog.repository;
 
 import org.example.dronelog.model.MissaoVoo;
+import org.example.dronelog.model.StatusMissao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface MissaoVooRepository extends JpaRepository<MissaoVoo, Long> {
 
     // TODO: definir as consultas necessárias para acompanhamento das missões.
+    List<MissaoVoo> findByStatus(StatusMissao statusMissao);
+    List<MissaoVoo> findByLocalOperacao (String localOperacao);
+    List<MissaoVoo> findByDataPrevista(LocalDate dataPrevista);
+
+
     Optional<MissaoVoo> findByPilotoIdPiloto(Long idPiloto);
     List<MissaoVoo> findByPilotoNomeEqualsIgnoreCase(String nomePiloto);
+
     Optional<MissaoVoo> findByDroneIdDrone(Long idDrone);
+    List<MissaoVoo> findByDroneIdentificador(String droneIdentificador);
 }
--- src/main/java/org/example/dronelog/service/MissaoVooService.java
+++ src/main/java/org/example/dronelog/service/MissaoVooService.java
@@ -18,46 +18,80 @@ public class MissaoVooService {
     //TODO: completar ou alterar.
     private final MissaoVooRepository missaoVooRepository;
     private final PilotoService pilotoService;
+    private final DroneService droneService;
 
 
-    public MissaoVooService(MissaoVooRepository missaoVooRepository, PilotoService pilotoService) {
+    public MissaoVooService(MissaoVooRepository missaoVooRepository, PilotoService pilotoService, DroneService droneService) {
         this.missaoVooRepository = missaoVooRepository;
         this.pilotoService = pilotoService;
-
+        this.droneService = droneService;
     }
 
     public List<MissaoVooResponseDTO> listar(StatusMissao status, String localOperacao, LocalDate dataPrevista) {
         // TODO: construir a listagem e os filtros necessários.
-        if (!status.toString().isBlank()){
+        if (status != null){
             return buscarStatus(status);
-        } else if (!localOperacao.isBlank()) {
+        } else if (localOperacao != null) {
             return buscarLocalOperacao(localOperacao);
-        } else if (!dataPrevista.toString().isBlank()) {
+        } else if (dataPrevista != null) {
             return buscarDataPrevista(dataPrevista);
         }else {
             return missaoVooRepository.findAll().stream().map(this::toResponse).toList();
         }
     }
 
+    public List<MissaoVooResponseDTO> buscarStatus(StatusMissao status){
+        return missaoVooRepository.findByStatus(status).stream().map(this::toResponse).toList();
+    }
 
+    public List<MissaoVooResponseDTO> buscarLocalOperacao(String localOperacao){
+        return missaoVooRepository.findByLocalOperacao(localOperacao).stream().map(this::toResponse).toList();
+    }
+
+    public List<MissaoVooResponseDTO> buscarDataPrevista(LocalDate dataPrevista){
+        return missaoVooRepository.findByDataPrevista(dataPrevista).stream().map(this::toResponse).toList();
+    }
 
     public MissaoVooResponseDTO buscarPorId(Long id) {
-        // TODO: localizar e converter a missão.
-        return null;
+        MissaoVoo missao = buscarMissao(id);
+        return toResponse(missao);
     }
 
     public MissaoVooResponseDTO cadastrar(MissaoVooRequestDTO dto) {
-        // TODO: montar a entidade, preencher dados simples e resolver vínculos.
-        return null;
+        // TODO: montar a entidade, preencher dados simples e resolver vínculos. - FEITO, eu acho
+
+        MissaoVoo missaoVoo = new MissaoVoo();
+        missaoVoo.setTitulo(dto.titulo());
+        missaoVoo.setLocalOperacao(dto.localOperacao());
+        missaoVoo.setDataPrevista(dto.dataPrevista());
+        missaoVoo.setAreaMapeadaKm2(dto.areaMapeadaKm2());
+        missaoVoo.setStatus(dto.status());
+        missaoVoo.setPiloto(pilotoService.buscarPiloto(dto.idPiloto()));
+        missaoVoo.setDrone(droneService.buscarDrone(dto.idDrone()));
+
+        MissaoVoo salvo = missaoVooRepository.save(missaoVoo);
+        return toResponse(salvo);
     }
 
     public MissaoVooResponseDTO atualizar(Long id, MissaoVooRequestDTO dto) {
         // TODO: recuperar o registro existente e aplicar alterações permitidas.
-        return null;
+        MissaoVoo missaoVoo = new MissaoVoo();
+        missaoVoo.setIdMissao(id);
+        missaoVoo.setTitulo(dto.titulo());
+        missaoVoo.setLocalOperacao(dto.localOperacao());
+        missaoVoo.setDataPrevista(dto.dataPrevista());
+        missaoVoo.setAreaMapeadaKm2(dto.areaMapeadaKm2());
+        missaoVoo.setStatus(dto.status());
+        missaoVoo.setPiloto(pilotoService.buscarPiloto(dto.idPiloto()));
+        missaoVoo.setDrone(droneService.buscarDrone(dto.idDrone()));
+
+        MissaoVoo atualizado = missaoVooRepository.save(missaoVoo);
+        return toResponse(atualizado);
     }
 
     public void deletar(Long id) {
-        // TODO: remover o registro correto.
+        MissaoVoo missaoVoo = buscarMissao(id);
+        missaoVooRepository.delete(missaoVoo);
     }
 
     private MissaoVoo buscarMissao(Long id) {
@@ -67,6 +101,17 @@ public class MissaoVooService {
 
     private MissaoVooResponseDTO toResponse(MissaoVoo missao) {
         // TODO: transformar a entidade em resposta sem expor objetos inteiros.
-        return null;
+        return new MissaoVooResponseDTO(
+                missao.getIdMissao(),
+                missao.getTitulo(),
+                missao.getLocalOperacao(),
+                missao.getDataPrevista(),
+                missao.getAreaMapeadaKm2(),
+                missao.getStatus(),
+                missao.getPiloto().getIdPiloto(),
+                missao.getPiloto().getNome(),
+                missao.getDrone().getIdDrone(),
+                missao.getDrone().getIdentificador()
+        );
     }
 }
